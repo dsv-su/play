28e2afe0a783363e338d84f7a4b22239fb770edc
orkit <ryan@dias.se>
2020-09-28 15:17:38 +0200

    Fixed responsive layout

--- resources/views/player/index.blade.php
+++ resources/views/player/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="{{ asset('./images/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('./css/reset.css') }}">
-    <link rel="stylesheet" href="{{ asset('./css/style2.css') }}">
+    <link rel="stylesheet" href="{{ asset('./css/style.css') }}">
     <link rel="stylesheet" href="{{asset('./css/fontawesome/css/all.css')}}" >
 
     <title>{{ $video->title }}</title>
@@ -221,6 +221,6 @@
     </defs>
 </svg>
 
-<script src="{{asset('./js/player2.js')}}" defer></script>
+<script src="{{asset('./js/player.js')}}" defer></script>
 </body>
 </html>
