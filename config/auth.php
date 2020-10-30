f91c882ab9a71902eb26729c3bf66518996f5e63
orkit <ryan@dias.se>
2020-10-30 11:21:45 +0100

    Restored auth setting

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'shibboleth',
+            'driver' => 'eloquent',
             'model' => App\User::class,
         ],
 
