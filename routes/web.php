0b9c130072d78dcb8f77a46c59720a9ad9ac60bd
orkit <ryan@dias.se>
2021-01-20 15:54:38 +0100

    Added route dev testing

--- routes/web.php
+++ routes/web.php
@@ -82,6 +82,7 @@ Route::middleware('entitlements')->group(function () {
     //Testing routes
     Route::get('/upload_destroy/{id}', 'AdminController@destroy_upload')->name('upload_delete');
     Route::get('/download_destroy/{id}', 'AdminController@destroy_download')->name('download_delete');
+    Route::get('/dev_destroy/{id}', 'AdminController@dev_destroy');
     Route::get('/php', 'TestController@php')->name('php');
     Route::get('/server', 'TestController@server')->name('server');
     Route::get('/daisy', 'TestController@daisy')->name('daisy');
