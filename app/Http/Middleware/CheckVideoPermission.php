318839a93d9414538fc4b89c5ca1665357402ad0
orkit <ryan@dias.se>
2021-03-16 14:55:24 +0100

    Adding dev mode middleware

--- app/Http/Middleware/CheckVideoPermission.php
+++ app/Http/Middleware/CheckVideoPermission.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Services\AuthHandler;
 use App\VideoPermission;
 use Closure;
 use Illuminate\Http\Request;
@@ -17,11 +18,19 @@ class CheckVideoPermission
      */
     public function handle(Request $request, Closure $next)
     {
+        $system = new AuthHandler();
+        $system = $system->authorize();
         $permission = VideoPermission::where('video_id', $request->p)->first();
-        
-        if(!($permission->permission_id == 4)) {
-            return redirect()->guest(route('sulogin'));
+        if(!$request->server('REMOTE_USER')) {
+            if($permission->permission_id != 4) {
+                if($system->global->app_env == 'local') {
+                    return $next($request);
+                } else {
+                    return redirect()->guest(route('sulogin'));
+                }
+            }
         }
+
         return $next($request);
     }
 }
