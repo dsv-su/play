f078e6f1628fcbe9644d735164006906d9c529c9
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2021-09-24 12:02:46 +0300

    video icons

--- resources/views/home/video.blade.php
+++ resources/views/home/video.blade.php
@@ -5,22 +5,29 @@
              style="background-image: @if ($video->visability == false) linear-gradient(rgba(255,255,255,0.7), rgba(255,255,255,0.7)), @endif url({{ asset($video->thumb)}}); height:200px;">
             <div class="title">{{ $video->title }}</div>
             <!-- Group Permissions -->
-            @if($permissions ?? '')
-                @foreach($permissions as $permission)
-                    @if($video->id == $permission->video_id && $permission->type == 'private')
-                        <div class="permission"><i class="fas fa-user-lock"></i></div>
-                    @endif
-                    @if($video->id == $permission->video_id && $permission->type == 'external')
-                        <div class="permission"><i class="fas fa-globe"></i></div>
-                    @endif
-                @endforeach
-            @endif
+            <div class="icons">
+                @if($permissions ?? '')
+                    @foreach($permissions as $permission)
+                        @if($video->id == $permission->video_id && $permission->type == 'private')
+                            <div class="permission"><i class="fas fa-user-lock"></i></div>
+                        @endif
+                        @if($video->id == $permission->video_id && $permission->type == 'external')
+                            <div class="permission"><i class="fas fa-globe"></i></div>
+                        @endif
+                    @endforeach
+                @endif
             <!-- end Group Permission -->
-            <!-- Visability -->
-            @if($video->visability == false)
-                <div class="visability"><i class="far fa-eye-slash"></i></div>
-                <div class="d-flex justify-content-center h-100"><div class="d-inline alert alert-secondary m-auto" role="alert">{{ __("Presentation hidden") }}</div></div>
-                    @endif
+                <!-- Visability -->
+                @if($video->visability == false)
+                    <div class="visability"><i class="far fa-eye-slash"></i></div>
+                @endif
+            </div>
+            @if ($video->visability == false)
+                <div class="d-flex justify-content-center h-100">
+                    <div class="d-inline alert alert-secondary m-auto"
+                         role="alert">{{ __("Presentation hidden") }}</div>
+                </div>
+            @endif
             <p class="p-1"> {{$video->duration}} </p>
         </div>
     </a>
@@ -53,50 +60,54 @@
                 @endforeach
             </p>
         @endif
-        @if (isset($manage) && $manage)
-            <div class="d-inline">
+        <div class="d-flex float-right clearfix">
+            <div class="dropdown ml-1" data-toggle="tooltip" data-placement="top"
+                 title="{{ __("Download presentation") }}">
+                <a class="btn btn-outline-info btn-sm mb-1" href="#" role="button" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-download"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                    <form method="post" action="{{route('download', $video->id)}}">
+                        @csrf
+                        @foreach(json_decode($video->sources, true) as $source)
+                            @if(is_array($source['video']) && $loop->first)
+                                @foreach($source['video'] as $key => $source)
+                                    <button class="dropdown-item btn btn-outline-primary btn-sm" name="res"
+                                            value="{{$key}}"><i class="fas fa-download"></i>
+                                        {{$key}}p
+                                    </button>
+                                @endforeach
+                            @elseif($loop->first)
+                                <button class="dropdown-item btn btn-outline-primary btn-sm" name="res" value="999">
+                                    <i class="fas fa-download"></i> 720p
+                                </button>
+                            @endif
+                        @endforeach
+                    </form>
+                </div>
+            </div>
+            @if (isset($manage) && $manage)
+                <div class="ml-1">
                 <a href="{{route('presentation_edit', $video->id)}}" data-toggle="tooltip"
                    title="{{ __('Edit Presentation') }}" class="btn btn-outline-primary btn-sm mb-1"><i
                             class="far fa-edit"></i></a>
+                </div>
                 {{--}}
                 <a href="{{route('edit_permission', $video->id)}}" data-toggle="tooltip"
                    title="{{ __("Change rights for presentation") }}" class="btn btn-outline-secondary btn-sm mb-1"><i
                             class="far fa-hand-paper"></i></a>
                 {{--}}
-                <div class="dropdown d-inline" data-toggle="tooltip" data-placement="top"
-                     title="{{ __("Download presentation") }}">
-                    <a class="btn btn-outline-info btn-sm mb-1" href="#" role="button" data-toggle="dropdown"
-                       aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-download"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                        <form method="post" action="{{route('download', $video->id)}}">
-                            @csrf
-                            @foreach(json_decode($video->sources, true) as $source)
-                                @if(is_array($source['video']) && $loop->first)
-                                    @foreach($source['video'] as $key => $source)
-                                        <button class="dropdown-item btn btn-outline-primary btn-sm" name="res"
-                                                value="{{$key}}"><i class="fas fa-download"></i>
-                                            {{$key}}p
-                                        </button>
-                                    @endforeach
-                                @elseif($loop->first)
-                                    <button class="dropdown-item btn btn-outline-primary btn-sm" name="res" value="999">
-                                        <i class="fas fa-download"></i> 720p
-                                    </button>
-                                @endif
-                            @endforeach
-                        </form>
-                    </div>
-                </div>
+                <div class="ml-1">
                 <form>
                     <meta name="csrf-token" content="{{ csrf_token() }}">
                     <a href="#" data-toggle="tooltip" title="{{ __("Delete presentation") }}"
-                       class="btn btn-outline-danger btn-sm ml-1 mb-1 float-right delete"><i
+                       class="btn btn-outline-danger btn-sm float-right delete"><i
                                 class="far fa-trash-alt"></i></a>
                 </form>
-            </div>
-            <p class="m-0"><small>{{$video->id}}</small></p>
-        @endif
+                </div>
+            @endif
+        </div>
+        <p class="m-0"><small>{{$video->id}}</small></p>
     </div>
 </div>
