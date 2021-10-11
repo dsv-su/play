51bc455075c667ae09605e20e4fa0521f261df9f
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2021-10-11 16:25:13 +0300

    tiny fix

--- resources/views/home/mediasite.blade.php
+++ resources/views/home/mediasite.blade.php
@@ -2,19 +2,20 @@
 @section('content')
     <script type="text/javascript" src="{{asset('daterangepicker/moment.min.js')}}"></script>
     <script type="text/javascript" src="{{asset('daterangepicker/daterangepicker.min.js')}}"></script>
-    <link rel="stylesheet" type="text/css" href="{{asset('daterangepicker/daterangepicker.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('daterangepicker/daterangepicker.css')}}"/>
     <form action="{{ route('mediasiteCourseDownload') }}" method="POST">
         @csrf
         @method('POST')
         Choose a course folder to download:<br/><br/>
         Course: <select id="course" name="course">
-            <option disabled selected value> -- select an option -- </option>
+            <option disabled selected value> -- select an option --</option>
             @foreach($mediasitecourses as $folder)
                 <option value="{{$folder['id']}}">{{$folder['name']}}</option>
             @endforeach
         </select><br>
-        Dates: <input type="text" name="daterange" class="w-50" /><br/>
-        Designation: <input type="text" class="col-3" id="designation" name="designation" placeholder="Designation" required>
+        Dates: <input type="text" name="daterange" class="w-50"/><br/>
+        Designation: <input type="text" class="col-3" id="designation" name="designation" placeholder="Designation"
+                            required>
         <div id="todownload" class="alert alert-info w-50 font-1rem" role="alert">No presentations to fetch</div>
         <input type="submit" class="btn btn-sm btn-primary" value="Download"/><br/><br/>
     </form>
@@ -23,12 +24,12 @@
         @csrf
         Choose a user folder to download:<br/><br/>
         Username: <select id="user" name="user">
-            <option disabled selected value> -- select an option -- </option>
+            <option disabled selected value> -- select an option --</option>
             @foreach($users as $folder)
                 <option value="{{$folder['id']}}">{{$folder['name']}}</option>
             @endforeach
         </select><br/>
-        Dates: <input type="text" name="daterange" class="w-50" /><br/>
+        Dates: <input type="text" name="daterange" class="w-50"/><br/>
         <br/>
         <input type="submit" class="btn btn-sm btn-primary" value="Download"/><br/><br/>
     </form>
@@ -37,12 +38,12 @@
         @csrf
         Choose a separate folder to download:<br/><br/>
         Folder: <select id="recording" name="recording">
-            <option disabled selected value> -- select an option -- </option>
+            <option disabled selected value> -- select an option --</option>
             @foreach($recordings as $folder)
                 <option value="{{$folder['id']}}">{{$folder['name']}}</option>
             @endforeach
         </select><br/>
-        Dates: <input type="text" name="daterange" class="w-50" /><br/>
+        Dates: <input type="text" name="daterange" class="w-50"/><br/>
         <br/>
         <input type="submit" class="btn btn-sm btn-primary" value="Download"/><br/><br/>
     </form>
@@ -51,12 +52,12 @@
         @csrf
         Choose other folders to download:<br/><br/>
         Folder: <select id="other" name="other">
-            <option disabled selected value> -- select an option -- </option>
+            <option disabled selected value> -- select an option --</option>
             @foreach($other as $folder)
                 <option value="{{$folder['id']}}">{{$folder['name']}}</option>
             @endforeach
         </select><br/>
-        Dates: <input type="text" name="daterange" class="w-50" /><br/>
+        Dates: <input type="text" name="daterange" class="w-50"/><br/>
         <br/>
         <input type="submit" class="btn btn-sm btn-primary" value="Download"/><br/><br/>
     </form>
@@ -89,12 +90,10 @@
                 data: $(this).serialize(),
                 success: function (data) {
                     var html = 'Presentations to be downloaded: <br>';
-                    console.log(JSON.parse(data.presentations));
                     $.each(JSON.parse(data.presentations), function (i, p) {
-                        console.log(p);
-                       html += p.name + ' (id' + p.id + ')</br>';
+                        html += p.name + ' (id: ' + p.id + ')</br>';
                     });
-                   $('#todownload').html(html);
+                    $('#todownload').html(html);
                 }
             });
         });
