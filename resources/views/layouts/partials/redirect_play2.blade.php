f1b71595e744fa6b18b5839109adc8fdba293e6b
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2022-03-16 15:55:31 +0100

    style play2 notice

--- resources/views/layouts/partials/redirect_play2.blade.php
+++ resources/views/layouts/partials/redirect_play2.blade.php
@@ -1,14 +1,14 @@
 <section class="global-alert-banner js-banner-type-message p-2 alert alert-info alert-dismissible fade m-0 show" role="dialog" aria-label="Play2">
     <div class="container">
         <div class="row no-gutters">
-            <div class="col d-flex align-items-center">
-                <div class="d-none d-sm-block"><i class="fa-solid fa-share-from-square"></i></div>
-                <div class="webb2021-article p-0 p-lg-2">
+            <div class="col d-flex align-items-center justify-content-center">
+                <div class="d-none d-sm-inline-block"><i class="fa-solid fa-share-from-square"></i></div>
+                <div class="webb2021-article p-0 p-lg-2 d-inline-block">
                     <a href="https://play2.dsv.su.se">{{__("If you are missing presentations from before 20th of March, click here to reach the old site!")}}</a>
                 </div>
-                <div class="ml-auto pl-3 h-100 d-flex align-items-center">
-                    <button class="button-remove-style js-alert-banner-dismiss-btn pl-4 h-100" type="button" data-dismiss="alert"  aria-label="{{__('Close')}}">
-                        <span class="far fa-times-circle align-middle" aria-hidden="true"> </span>
+                <div class="h-100 d-flex align-items-center">
+                    <button class="button-remove-style js-alert-banner-dismiss-btn h-100 p-1" type="button" data-dismiss="alert"  aria-label="{{__('Close')}}">
+                        <span class="far fa-times-circle" aria-hidden="true"> </span>
                     </button>
                 </div>
             </div>
