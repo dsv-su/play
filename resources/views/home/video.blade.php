1cb36e115955a2ee9c5e70c9b066728f94a289fa
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2022-03-18 11:47:06 +0100

    css

--- resources/views/home/video.blade.php
+++ resources/views/home/video.blade.php
@@ -37,7 +37,7 @@
             @endif
     </div>
     <a href="{{ route('player', ['video' => $video]) }}">
-        <div class="card-header position-relative" style="background-image: @if ($video->hidden) linear-gradient(rgba(255,255,255,0.7), rgba(255,255,255,0.7)), @endif url({{ asset($video->thumb)}}); width: 100%; height: 0px; object-fit: contain; background-repeat: round; background-position: 50% 50%;">
+        <div class="card-header position-relative" style="background-image: @if ($video->hidden) linear-gradient(rgba(255,255,255,0.7), rgba(255,255,255,0.7)), @endif url({{ asset($video->thumb)}}); width: 100%; height: 0px; object-fit: contain; background-repeat: no-repeat; background-position: 50% 50%;">
             <!-- Icons -->
             <div class="icons m-1">
                 <!-- Group Permissions icons-->
