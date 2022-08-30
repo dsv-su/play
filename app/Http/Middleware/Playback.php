db019d2f5e5b1054a9a85c594b210e92d23e8ac9
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2022-08-30 09:59:43 +0200

    fix null courses

--- app/Http/Middleware/Playback.php
+++ app/Http/Middleware/Playback.php
@@ -18,8 +18,8 @@ class Playback
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
@@ -29,17 +29,19 @@ class Playback
          */
         $system = new AuthHandler();
         $system = $system->authorize();
-        if($video = Video::find(basename($request->getUri()))) {
-
-        }
-        else {
+        if ($video = Video::find(basename($request->getUri()))) {
+        } else {
             $video = Video::find($request->p);
         }
 
+        if (!$video) {
+            return redirect()->route('home');
+        }
+
         //Check if user is through SU SSO
-        if(!$request->server('REMOTE_USER')) {
+        if (!$request->server('REMOTE_USER')) {
             //Local dev enviroment
-            if($system->global->app_env == 'local') {
+            if ($system->global->app_env == 'local') {
                 return $next($request);
             }
             //If Shibboleth session is missing
@@ -48,16 +50,16 @@ class Playback
             $permission = VideoPermission::where('video_id', $video->id)->firstOrFail();
             if ($permission->permission_id == 4) {
                 //Presentation is public
-                if($video->visibility) {
+                if ($video->visibility) {
                     //Presentation visibility is set to default
 
                     //Check course association
-                    if(!count($video->courses()) > 0) {
+                    if (!count($video->courses()) > 0) {
                         return $next($request);
                     }
 
                     $coursesetting = new CourseSettingVisibility();
-                    if($coursesetting->check($video)) {
+                    if ($coursesetting->check($video)) {
                         //Coursesetting is set to default visibility
                         return $next($request);
                     }
@@ -67,26 +69,25 @@ class Playback
             //Check if coursesettings is public
             $course_permission = new CourseSettingPublic();
             $coursesetting = new CourseSettingVisibility();
-            if($course_permission->check($video) == 4) {
+            if ($course_permission->check($video) == 4) {
                 //Course is public
-                if($coursesetting->check($video)) {
+                if ($coursesetting->check($video)) {
                     return $next($request);
                 }
             }
 
             return redirect()->route('home');
-        }
-        else {
+        } else {
             //Shibboleth
 
             //If user is Admin
             $admin = new AdminCheck();
-            if($admin->check()) {
+            if ($admin->check()) {
                 return $next($request);
             }
 
             //Check if video belongs to course
-            if(count($video->courses()) >= 1) {
+            if (count($video->courses()) >= 1) {
 
                 //Associated to one or more course
                 $staff = new StaffCheck();
@@ -95,29 +96,29 @@ class Playback
 
                 // Check if user is courseAdmin
                 // (1) First check if user is staff
-                if($staff->check()) {
+                if ($staff->check()) {
                     // (2) If user is staff check if user is courseadmin
                     $courseadmin = new \App\Services\Course\CourseAdmin();
-                    if($courseadmin->check($_SERVER['REMOTE_USER'], $video)) {
+                    if ($courseadmin->check($_SERVER['REMOTE_USER'], $video)) {
                         return $next($request);
                     }
                 }
 
                 // (2) Check if user exist in coursesettings list
-                if($courseadmin_list->check($_SERVER['REMOTE_USER'], $video)) {
+                if ($courseadmin_list->check($_SERVER['REMOTE_USER'], $video)) {
                     //User exist in CourseAdmin List
                     return $next($request);
                 }
 
                 // (3) Check if Individual presentation settings allows playback
                 //Check if individual permissions has been set for presentation
-                if($individuals = $video->ipermissions ?? false) {
-                    foreach($individuals as $iper) {
+                if ($individuals = $video->ipermissions ?? false) {
+                    foreach ($individuals as $iper) {
                         //Check if user is listed
                         $username = substr($_SERVER['REMOTE_USER'], 0, strpos($_SERVER['REMOTE_USER'], "@"));
-                        if($iper->username == $username) {
+                        if ($iper->username == $username) {
                             //Check if user has set permissions
-                            if(in_array($iper->permission, ['read', 'edit', 'delete'])) {
+                            if (in_array($iper->permission, ['read', 'edit', 'delete'])) {
                                 return $next($request);
                             }
                         }
@@ -126,12 +127,12 @@ class Playback
                 }
 
                 // (4) Check if Course visibility allows playback
-                if($coursesetting->check($video)) {
+                if ($coursesetting->check($video)) {
                     return $next($request);
                 }
 
                 // (5) Check if Presentation visibility allows playback
-                if($video->visibility) {
+                if ($video->visibility) {
                     return $next($request);
                 }
 
@@ -140,19 +141,18 @@ class Playback
             } else {
 
                 //Not associated to a course
-                if($video->visibility) {
+                if ($video->visibility) {
                     return $next($request);
-                }
-                else {
+                } else {
 
                     //Check individual settings
                     //Check if individual permissions has been set for presentation
-                    if($individuals = $video->ipermissions ?? false) {
-                        foreach($individuals as $iper) {
+                    if ($individuals = $video->ipermissions ?? false) {
+                        foreach ($individuals as $iper) {
                             //Check if user is listed
-                            if($iper->username == $_SERVER['REMOTE_USER']) {
+                            if ($iper->username == $_SERVER['REMOTE_USER']) {
                                 //Check if user has set permissions
-                                if(in_array($iper->permission, ['read', 'edit', 'delete'])) {
+                                if (in_array($iper->permission, ['read', 'edit', 'delete'])) {
                                     return $next($request);
                                 }
                             }
@@ -161,7 +161,7 @@ class Playback
                     return redirect()->route('home');
                 }
             }
-        //End Shibboleth
+            //End Shibboleth
         }
 
         //If none of the above handles the request
