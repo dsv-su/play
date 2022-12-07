be79e30fc216435c9289a8e727d86cead0003db7
orkit <ryan@dias.se>
2022-12-07 13:05:07 +0100

    Modified Savebar bg color

--- resources/views/layouts/partials/bulkbar.blade.php
+++ resources/views/layouts/partials/bulkbar.blade.php
@@ -1,24 +1,20 @@
 <!-- Save bar -->
 <div class="container-fluid px-2 play_savebar" id="bulkcontainer" style="display: none;">
-    <div class="d-flex justify-content-center row">
-        <div class="col-md-12">
-            <div class="bar p-3 bg-white">
-
-                <div class="d-flex row no-gutters">
-                    <div class="col-md-4 border-right">
-                        <div class="d-flex flex-column align-items-center">
-                            <span class="d-block" id="bulk">{{__("Bulk edit")}}</span>
-                        </div>
-                    </div>
-                    <div class="col-md-8">
-                        <form class="d-flex flex-column justify-content-end" id="bulkediting" method="post" action="{{route('edit.bulk.show')}}">
-                            @csrf
-                            @method('GET')
-                            <input type="submit" class="btn btn-lg btn-outline-primary m-auto"><br><br>
-                        </form>
-                    </div>
-
+    <div class="bar p-3 su-header-container__primary">
+        <div class="d-flex row no-gutters justify-content-end">
+            {{--}}
+            <div class="col-md-4 border-right">
+                <div class="d-flex flex-column align-items-center">
+                    <span class="d-block" id="bulk">{{__("Bulk edit")}}</span>
                 </div>
+            </div>
+            {{--}}
+            <div class="col-md-4">
+                <form class="d-flex flex-column justify-content-end" id="bulkediting" method="post" action="{{route('edit.bulk.show')}}">
+                    @csrf
+                    @method('GET')
+                    <input type="submit" class="btn btn-lg btn-outline-light m-auto"><br><br>
+                </form>
             </div>
         </div>
     </div>
--- resources/views/livewire/bar/save.blade.php
+++ resources/views/livewire/bar/save.blade.php
@@ -1,15 +1,17 @@
 <!-- Save bar -->
 <div class="container-fluid px-2 play_savebar">
-    <div class="bar p-3 bg-white">
-        <div class="d-flex row no-gutters">
+    <div class="bar p-3 su-header-container__primary">
+        <div class="d-flex row no-gutters justify-content-end">
+            {{--}}
             <div class="col-md-4 border-right">
                 <div class="d-flex flex-column align-items-center">
                     <span class="d-block" id="bulk">{{__("Save")}}</span>
                 </div>
             </div>
-            <div class="col-md-8">
-                <div class="d-flex flex-row justify-content-end" id="bulkediting">
-                    <button type="submit" id="submit" class="btn btn-lg btn-outline-primary m-auto">{{ __("Save") }}</button>
+            {{--}}
+            <div class="col-md-4">
+                <div class="d-flex flex-row" id="bulkediting">
+                    <button type="submit" id="submit" class="btn btn-lg btn-outline-light m-auto">{{ __("Save") }}</button>
                 </div>
             </div>
         </div>
