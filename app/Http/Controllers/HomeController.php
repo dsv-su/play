eabcd57307012e9d68b5663fa344c9a9736ad253
orkit <ryan@dias.se>
2023-05-17 15:44:05 +0200

    Fixed bugg in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -123,7 +123,7 @@ class HomeController extends Controller
         // All courses (tab 3)
         if($this->administrator()) {
             //Administrator full visibility
-            $data['allpaginated'] = Video::with('category', 'video_course.course')->latest('creation')->fastPaginate(24, ['*'], 'all')->onEachSide(1);
+            $data['allpaginated'] = Video::with('category', 'video_course.course')->where('state', true)->latest('creation')->fastPaginate(24, ['*'], 'all')->onEachSide(1);
         } else {
             //Non administrator restricted visibility
             $data['allpaginated'] = Video::with('category', 'video_course.course')->where('visibility', true)->where('state', true)->latest('creation')->fastPaginate(24, ['*'], 'all')->onEachSide(1);
