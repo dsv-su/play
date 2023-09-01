44a6c3c3b7765a7a9ab0d9e96f1863bac80b29a9
orkit <ryan@dias.se>
2023-09-01 15:23:47 +0200

    Deactivated message banner

--- app/Services/Stream/StreamsStore.php
+++ app/Services/Stream/StreamsStore.php
@@ -19,7 +19,7 @@ class StreamsStore extends Model
 
     public function streams()
     {
-        //Check if presentation has been edited
+        //Check if presentation has been edited override source setting
         if(!$presentation = ManualPresentation::where('pkg_id', $this->video->id)->where('type', 'edit')->where(function($query) {
             $query->where('status', 'sent')->orWhere('status', 'completed');})->latest()->first()) {
             if(count($this->streams)>0) {
