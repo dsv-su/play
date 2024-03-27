2e0fd00cb477ca0c5113d477c98ba23433aba2e6
orkit <ryan@dias.se>
2024-03-27 10:25:59 +0100

    Prepared solution for bug issue #138

--- app/Services/Stream/StreamsStore.php
+++ app/Services/Stream/StreamsStore.php
@@ -55,12 +55,65 @@ class StreamsStore extends Model
                     StreamResolution::where('stream_id', $stream->id)->delete();
                     $stream->delete();
                 }
-                //Update stream resolutions
-
-                //end update
-
             }
         } //end presentation edit
     }
 
 }
+
+/***
+ * Updated class
+ * Backend handles hidden streams
+ */
+
+/*class StreamsStore extends Model
+{
+    protected $video;
+
+    public function __construct($request, $video)
+    {
+        $this->sources = $request->input('package.sources');
+        $this->video = $video;
+    }
+
+    public function streams()
+    {
+
+        if (empty($this->sources)) {
+            $this->deleteAllStreams();
+            return;
+        }
+
+        foreach ($this->sources as $key => $source) {
+            if (empty($source)) {
+                continue;
+            }
+
+            $stream = $this->updateStream($key, $source);
+            $this->updateStreamResolutions($stream, $source['video'] ?? []);
+        }
+    }
+
+    protected function updateStream($name, $source)
+    {
+        return Stream::updateOrCreate(
+            ['video_id' => $this->video->id, 'name' => $name],
+            ['poster' => $source['poster'] ?? '', 'audio' => $source['playAudio'] ?? false]
+        );
+    }
+
+    protected function updateStreamResolutions($stream, $videoUrls)
+    {
+        foreach ($videoUrls as $resolution => $url) {
+            StreamResolution::updateOrCreate(
+                ['stream_id' => $stream->id, 'resolution' => $resolution],
+                ['filename' => $url]
+            );
+        }
+    }
+
+    protected function deleteAllStreams()
+    {
+        $this->video->streams()->delete();
+    }
+}*/
