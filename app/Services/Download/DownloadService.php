eecfca8d63a55fe6f69d5207e0de321d97faaf6a
orkit <ryan@dias.se>
2025-10-21 17:09:41 +0200

    Debugging log

--- app/Services/Download/DownloadService.php
+++ app/Services/Download/DownloadService.php
@@ -63,9 +63,10 @@ class DownloadService
         $posterNames = $resolver->posternames();
 
         // Create directories
+        \Log::notice('starting directory creation');
         Storage::disk(self::PUBLIC_DISK)->makeDirectory($dirVideo);
         Storage::disk(self::PUBLIC_DISK)->makeDirectory($dirPoster);
-
+        \Log::notice('end directory creation');
         // Download video files
         foreach ($videoNames as $name) {
             \Log::notice('starting file download');
