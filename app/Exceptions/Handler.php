c3c0fd95ad8f81c17b00cc693fa176b28bddffb1
orkit <ryan@dias.se>
2025-10-24 10:37:45 +0200

    Fixed bugg in schedueler

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Jobs\JobDevNotification;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        TokenExpiredException::class,
+        TokenBlacklistedException::class,
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->reportable(function (Throwable $e) {
+            // Create Notification Data
+            $exception = [
+                "server" => config('app.name'),
+                "name" => get_class($e),
+                "message" => $e->getMessage(),
+                "file" => $e->getFile(),
+                "line" => $e->getLine(),
+            ];
+
+            // Create a Job for Notification which will run after 5 seconds.
+            $job = (new JobDevNotification($exception))->delay(5);
+
+            // Dispatch Job and continue
+            dispatch($job);
+        });
+    }
+
+    /*
+     * Report or log an exception.
+     *
+     * @param  \Throwable  $exception
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public function report(Throwable $exception)
+    {
+        parent::report($exception);
+    }
+
+    /*
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        return parent::render($request, $exception);
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -9,11 +9,13 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::group(function () {
-    Schedule::command('adminhandler:truncate');
-    Schedule::command('upload:clear');
-    Schedule::command('uploads:clear');
-    Schedule::command('download:clear');
-    Schedule::command('update:courses');
-    Schedule::command('tokens:clear');
-})->dailyAt('21:00')->timezone('Europe/Stockholm');
+Schedule::dailyAt('21:00')
+    ->timezone('Europe/Stockholm')
+    ->group(function () {
+        Schedule::command('adminhandler:truncate');
+        Schedule::command('upload:clear');
+        Schedule::command('uploads:clear');
+        Schedule::command('download:clear');
+        Schedule::command('update:courses');
+        Schedule::command('tokens:clear');
+    });
