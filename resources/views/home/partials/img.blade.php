271342ca0b58aaaf954bfb4222bc85c4c5d327c1
orkit <ryan@dias.se>
2025-11-05 14:37:24 +0100

    Modified slide drag

--- resources/views/home/partials/img.blade.php
+++ resources/views/home/partials/img.blade.php
@@ -1,4 +1,4 @@
-<div class="relative aspect-video w-full overflow-hidden rounded-t-sm">
+<div class="relative aspect-video w-full overflow-hidden rounded-t-sm select-none pointer-events-none">
     <img
         class="absolute inset-0 h-full w-full object-cover
                @if($video->hidden) opacity-50 @endif"
--- resources/views/home/partials/presentation.blade.php
+++ resources/views/home/partials/presentation.blade.php
@@ -7,7 +7,7 @@
     <div class="relative">
 
         <a target="_blank" rel="noopener noreferrer" href="{{ route('player.show', ['video' => $video]) }}"
-           class="absolute inset-0 z-10" aria-label="Open video-{{$loop->index}}"></a>
+           class="absolute inset-0 z-10 hs-carousel-dragging:pointer-events-none" aria-label="Open video-{{$loop->index}}"></a>
 
         @include('home.partials.img')
         @include('home.partials.duration')
