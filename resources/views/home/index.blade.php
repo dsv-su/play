3f13a657a9370fc8c77c9ae63c44f3f9c5c53403
orkit <ryan@dias.se>
2025-11-28 15:43:35 +0100

    Fixed bugg in cachehandler

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,7 +8,7 @@
         'home.newpresentations',
         'home.mypresentations',
         'home.studypresentations',
-        'home.nextilearn'
+        'home.nextilearnpresentations'
         ];
 
     // Sanitize: keep only allowed values
