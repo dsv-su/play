a2848070838224bb498050fb36aeb97fb5d7fc79
orkit <ryan@dias.se>
2025-12-04 10:44:24 +0100

    Added link to iframe message

--- resources/views/errors/450.blade.php
+++ resources/views/errors/450.blade.php
@@ -2,4 +2,8 @@
 
 @section('title', __('Unauthorized'))
 @section('code', '401')
-@section('message', __('Please log in to DSVPlay (play.dsv.su.se) before accessing this presentation'))
+@section('message')
+    {!! 'Please log in to DSVPlay (<a href="https://play.dsv.su.se" style="color:#0d6efd; text-decoration: underline;">play.dsv.su.se</a>) before accessing this presentation' !!}
+@endsection
+
+
