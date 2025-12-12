803bdf6f02ea7e88cc9bb1c102dc93f8354d6fd4
orkit <ryan@dias.se>
2025-12-12 11:38:26 +0100

    Fixed copy paths in updatescript

--- app/Http/Controllers/MultiplayerController.php
+++ app/Http/Controllers/MultiplayerController.php
@@ -209,10 +209,11 @@ class MultiplayerController extends Controller
 
     public function multiplayer_ce(Request $request): View|RedirectResponse
     {
-        // Validate query params (?p=...&l=...)
+        // Validate query params
         $data = $request->validate([
             'p' => ['required', 'string'],   // presentation id
             'l' => ['nullable', 'int'],   // playlist id
+            's' => ['nullable', 'string'],   // default subtitle
         ]);
 
         // Build view data
